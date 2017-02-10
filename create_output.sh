26f64aad5781844aac8221cb123d2d0c1f28852b
sosherio <srogha2@uic.edu>
2017-02-10 12:50:08 -0600

    added the test data and script

---
+++ create_output.sh
@@ -0,0 +1,9 @@
+python msa.py small-data/para1-1.txt small-data/data-1.txt 1-1.txt;
+python msa.py small-data/para1-2.txt small-data/data-1.txt 1-2.txt;
+python msa.py large-data/para2-1.txt large-data/data-2.txt 2-1.txt;
+python msa.py large-data/para2-2.txt large-data/data-2.txt 2-2.txt;
+mkdir Basu_Nilanjana-662441607-Roghanchi_Sepideh-676765659;
+cp 1-1.txt Basu_Nilanjana-662441607-Roghanchi_Sepideh-676765659;
+cp 1-2.txt Basu_Nilanjana-662441607-Roghanchi_Sepideh-676765659;
+cp 2-1.txt Basu_Nilanjana-662441607-Roghanchi_Sepideh-676765659;
+cp 2-2.txt Basu_Nilanjana-662441607-Roghanchi_Sepideh-676765659;
